5ffa9922ad66850bf93a36861294d02d6afb535e
SuralePritesh <pritesh.surale@netprotector.net>
2024-09-30 11:31:41 +0530

    minor changes

--- test/test_example_test.py
+++ test/test_example_test.py
@@ -28,3 +28,11 @@ def test_class(default_student):
     assert default_student.major == 'ECE'
     assert default_student.years == 4
     
+
+
+
+def test_addition_function():
+    def add(x):
+        return x+x
+    
+    assert add(2) == 4
